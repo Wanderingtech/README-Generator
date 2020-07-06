f8e2125a785254449b1b62e1fcafae19db27d8c7
Wanderingtech <heather86lyn@gmail.com>
2020-07-05 22:24:34 -0700

    Basic README file write operation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+ console.log('Please answer to create your README.')
+
 // array of questions for user
 const questions = [
     {
@@ -21,7 +23,8 @@ const questions = [
     {
         type: 'input',
         name:'installation',
-        message: 'Please list the required steps for your project installation'
+        message: 'Please list the required steps for your project installation',
+        default: 'npm i'
     },
     {
         type: 'input',
@@ -29,9 +32,10 @@ const questions = [
         message: 'How will your project be used?'
     },
     {
-        type: 'confirm',
-        name: 'ifTest',
-        message: 'Is there a test written for your project?'
+        type: 'input',
+        name: 'test',
+        message: 'What tests does your project have?',
+        default: 'npm test'
     },
     {
         type: 'input',
@@ -66,7 +70,49 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then(function(answers){
+        let gitHubURL = `https:/github.com/${answers.gitHubUserName}`;
+        let newREADME = 
+        `# ${answers.title}
+        
+        ## Description
+        
+        ${answers.description}
+        
+        ## Table of Contents:
+        
+        *[License](#license)
+        *[Installation](#installation)
+        *[Usage](#usage)
+        *[Contribution](#contribution)
+        *[Links](#links)
+        
+        ## Installation
+        
+        Please run these commands to install dependancies for this project: ${answers.installation}
+        
+        ##Usage
+        
+        ${answers.usage}
+        
+        ## License
+        
+        This project is protected under the ${answers.license} license.
+        
+        ## Contributors
+        
+        ${answers.contributors}
 
+        ## Tests
+
+        ${answers.test}
+        
+        ## Questions
+        
+        If you have any questions regarding this project, you can find me at [${answers.gitHubUserName}](https://github.com/${response.gitHubUserName}) 
+        
+        and/or at ${response.email}.`
+    })
 }
 
 // function call to initialize program
